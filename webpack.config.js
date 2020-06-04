a304615c965d1a4bd9994b551f358e14794b996d
Sylvia Cung <sylvia.cung@liveramp.com>
2020-06-04 00:40:28 -0700

    add svg support

--- webpack.config.js
+++ webpack.config.js
@@ -4,28 +4,34 @@
 
 // there are 3 things webpack needs to know
 var path = require('path');
-var HtmlWebpackPlugin =  require('html-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	// 1. what the starting point of app is (root JS file)
-    entry : './app/index.js',
-    // 2. which transformations to make
-    module : {
-        rules : [
-            {test : /\.(js)$/, use:'babel-loader'}, //use babel JS compiler on all extensions that end with .js
-            {test : /\.css$/, use:['style-loader', 'css-loader']} //interprets @import and url() like import/require()
-        ]
-    },
-    // 3. where to output newly transformed code when webpack runs (it will end up in {AppName}/dist/index_bundle.js)
-    output : {
-        path : path.resolve(__dirname , 'dist'),
-        filename: 'index_bundle.js'
-    },
-    mode:'development',
-    plugins : [
-        new HtmlWebpackPlugin ({
-            template : 'app/index.html'
-        })
-    ]
-
-}
+	// 1. starting point webpack uses to start building dependency graph (root JS file)
+	entry: './app/index.jsx',
+	// 2. which transformations to make
+	module: {
+		//by default webpack only understands JS and JSON files, all other files need to be converted
+		//loaders transform non-JS source code so we can preprocess the files and add to dependency graph
+		rules: [
+			{ test: /\.(jsx)$/, use: 'babel-loader' }, //use babel JS compiler on all extensions that end with .jsx
+			{ test: /\.css$/, use: ['style-loader', 'css-loader'] }, //interprets @import and url() like import/require()
+			{ test: /\.svg$/, use: ['@svgr/webpack'] },
+		],
+	},
+	// 3. where to output newly transformed code when webpack runs (it will end up in {AppName}/dist/index_bundle.js)
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		// path: __dirname + 'app/assets/javascript',
+		filename: 'bundle.js',
+	},
+	mode: 'development',
+	//plugins are used for any other task that loaders can’t do (asset mgmt, optimization etc.)
+	plugins: [
+		//this plugin injects bundled JS as part of build process,
+		//otherwise we have to manually add it in index.html
+		new HtmlWebpackPlugin({
+			template: 'app/index.html',
+		}),
+	],
+};
