032cc15d261fd2dd869535a9e0115839ce3411f5
Sylvia Cung <sylvia.cung@liveramp.com>
2020-09-01 22:35:02 -0700

    fill in resume section

--- src/data.js
+++ src/data.js
@@ -4,39 +4,64 @@ export const data = [
 		jobTitle: 'Full Stack Engineer, Application Experience',
 		date: 'September 2019 - Present',
 		jobDescription: [
-			'Owned OKR for OAuth alpha release, designed and code complex React components, API layer and database tables',
-			'Certified Scrum master',
-			'Championed quality and site reliability initiatives',
+			'Owned OKRs for the alpha, then GA release, of a large OAuth project over two quarters: Worked with product and design teams to translate user stories into planned engineering tasks and managed cross-team dependencies; Designed the front-end components, API layer and database models, led QA process on the engineering side and was the primary code contributor for this project',
+			'Owned OKR to migrate design system: Audited front-end components that needed to be replaced to ensure feature parity in new system and set up migration process for other teams to follow',
+			'Scrum master for a new team of 6 engineers that consistently deliver high output. Successfully completed Certified ScrumMaster (CSM) certification through Scrum Alliance',
+			'Championed site reliability initiatives by attending Google Customer Reliability Engineering workshop and setting up process to determine SLIs and SLOs for a large client facing application with workflows touching 5+ engineering teams',
 		],
 	},
 	{
 		company: 'Sales Engineering @ LiveRamp',
 		jobTitle: 'Solutions Architect',
 		date: 'August 2018 - August 2019',
-		jobDescription: [],
+		jobDescription: [
+			'Technical product expert during sales cycle for high profile deals involving large brands and data providers',
+			'Helped shape product development by creating custom test workflows for new revenue generating use cases discovered through conversations with prospects',
+			'Resolved critical issues with how clients were using our systems, designed solutions to improve performance that helped recover potential revenue loss',
+			'Worked with business development, product management, engineering, legal, and privacy teams to get upsells and new deals closed',
+			'Consistently hit or exceeded quarterly goals and sales targets',
+		],
 	},
 	{
 		company: 'Professional Services @ Medallia',
 		jobTitle: 'Systems Analyst',
 		date: 'October 2017 - July 2018',
-		jobDescription: [],
+		jobDescription: [
+			'Lead Medallia’s products and custom reporting implementations for enterprise clients ($3-5M ACV)',
+			'Designed flexible solutions for different user types to view customer engagement opportunities for a large banking client, forecasted to result in 32% increase in client retention rate',
+			'Owned initiative to reduce technical debt by streamlining inefficient custom product implementations and coaching team members on JavaScript best practices',
+			'Lead quality feedback collection and data analysis for professional services organization and identified action plan to reduce number of customer facing quality incidents',
+		],
 	},
 	{
 		company: 'Product Ops @ LiveRamp',
 		jobTitle: 'Product Operations Manager',
 		date: 'February 2017 - October 2017',
-		jobDescription: [],
+		jobDescription: [
+			'Technical account owner responsibly for operational success of large revenue generating clients',
+			'Implemented and managed client data monetization workflows and advised clients how to best use our product to increase ad targeting reach and accuracy',
+			'Reduced total time spent on client accounts by 35% by increasing scalability in operational processes, using business judgment to push back on custom workflow requests and educating clients on self-service aspects of the platform',
+			'Investigated user journey from multiple sources of data, constructed and shared user stories to get buy-in to build client requested product improvements'
+		],
 	},
 	{
 		company: 'Professional Services @ Achievers',
 		jobTitle: 'Technical Consultant',
 		date: 'May 2015 - February 2017',
-		jobDescription: [],
+		jobDescription: [
+			"Lead on ~20 implementation projects for global and enterprise clients; helped break into EMEA market by launching Achievers' first Middle East client",
+			'Consistently outperformed team KPIs for overall client satisfaction, based on technical consultation/solution design, communication and product knowledge',
+			'Enabled customer success for enterprise clients during early adoption stages of API offering - designed/coded sample integration, demoed functionality to prospects, helped prioritize product roadmap and improved client-facing documentation which resulted in reduced number of daily inquiries to support queue'
+		],
 	},
 	{
 		company: 'Product @ Achievers',
 		jobTitle: 'Product Analyst (internship)',
 		date: 'Septemer 2014 - December 2014',
-		jobDescription: [],
+		jobDescription: [
+			'Helped product team make data-driven decisions, worked on project to build best approach to understand feature usage through data analysis and visualizations using Google Analytics, SQL Workbench, Birst and Confluence',
+			'Defined key metrics and created KPI dashboards to track insight on user journey for different segments, which led to identifying which features required improvement',
+			'Recognized opportunities in how product team can better leverage Google Analytics and participated in sprints to implement new email tracking solution',
+		],
 	},
 ];
